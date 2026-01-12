88d5e6c83e36a057b1bfbd2f3346167e88a0e061
FL-boop-blip <958101695@qq.com>
2026-01-13 00:04:05 +0800

    ex

--- example_code_cifar10.py
+++ example_code_cifar10.py
@@ -31,7 +31,7 @@ if __name__ == '__main__':
                              unbalanced_sgm=0,
                              data_path=data_path)
     # unbalanced
-    # data_obj = DatasetObject(dataset='CIFAR10', n_client=n_client, seed=23, rule='iid', unbalanced_sgm=0.3, data_path=data_path)
+    data_obj = DatasetObject(dataset='CIFAR10', n_client=n_client, seed=23, rule='iid', unbalanced_sgm=0.3, data_path=data_path)
 
     # Dirichlet (0.6)
     # data_obj = DatasetObject(dataset='CIFAR10', n_client=n_client, seed=20, unbalanced_sgm=0, rule='Drichlet', rule_arg=0.6, data_path=data_path)
